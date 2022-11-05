3da0c710ca587d746ad22bb5444e61ee25061122
UJJWAL TOMAR <102405945+itsAditiSaraswat@users.noreply.github.com>
2022-11-06 02:25:16 +0530

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// console.log(steg.encode('Hello How Are You?' , 'data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7'  ));
-
-// console.log(steg.decode('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABv0lEQVQ4T2NkYGD4ysDAwMgAAf+hNBOUDaL/QsVAFBuU/QsmBtL4E2rAHyQD2J64y8A0gmnhtqUMnMb2ICbI0H/IBmBzATYDWDiN7UEuZIFaCjYD5AKQASBBkAtggOmJuwzIFrhtUBeA1IO8AXI13IBfn7cs/s/rEwtyKiwMGJ64y4B4jAyMEDHhtqUgF8As/f314GYGbntfBsavBzf/ed+W+Y+Rm+8Pp53Pf3ZDW5AEyAXMUL/CwgBkAPOHOS1/vh/e9vfvi0cMPKEZDIxvmtP+/ziyDcn1DAyM3HyQKPn6CS7OLCHHANKEDEDqGJ+4y8CdjSJLJIfxVXnE/18XjhCpHFUZq4oOOBbALgAFyu/bFxl+nj/C8PvOFawGgrzBbmjDwKqiy8DrEwuPBRQvvJtQxvBt+zKsBoBsFJ+6AzUckKPu85bFDB8nV+L1DpdnFINQQRdcDdwL388eZHhbFY2iGWQjq6oehot4EysZ+COyUb3wNEgLJdpAsjI7n4AVPY+3wohC/tx2cDjAXfCqPIIBOTaQbUF3HSgwJRcewwzEjyumMnxZNZWBRVIOI7A+zGlh+LJ6Bjj1CaTUwL0KAG58pxl1mhXcAAAAAElFTkSuQmCC'));
-
 var imgdatauri;
 
 function readURL(input) {
@@ -25,6 +21,7 @@ function decode(input) {
           console.log(steg.decode(e.target.result));
         
         document.querySelector('#decoded').innerText = steg.decode(e.target.result);
+        document.getElementById('decoded').classList.add('p-2');
       };
     }
     reader.readAsDataURL(input.files[0]);
@@ -33,4 +30,10 @@ function decode(input) {
 
 function hideText() {
   document.querySelector("#image2").src = steg.encode(document.querySelector('#text').value, imgdatauri);
+
+  let a = document.getElementById('download-link');
+  a.href = `${steg.encode(document.querySelector('#text').value, imgdatauri)}`;
+
+  let b = document.getElementById('download-block');
+  b.classList.remove("hidden");
 }
